cacecd4548d56f927c5e06dd9127b1517821e133
Kirisan Manivannan <30753472+kirisanm@users.noreply.github.com>
2018-04-17 13:14:43 +0200

    Update

---
+++ Pointer/Pointer.cpp
@@ -0,0 +1,18 @@
+// Coded & developed by Kirisan Manivannan
+
+// Pointer
+
+#include <iostream>
+
+int main(){
+	
+	int num = 10;
+	int* ptr_num = &num;
+	
+	cout << num << endl; 		// answer: 10
+	cout << ptr_num << endl; 	// answer: num's adress
+	cout << *ptr_num << endl;	// answer: 10
+
+return 0;
+}
+
