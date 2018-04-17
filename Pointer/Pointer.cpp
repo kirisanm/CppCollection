bf90d4ff743b0ace1efd1c14ce7f3c2d50ff5f0a
Kirisan Manivannan <30753472+kirisanm@users.noreply.github.com>
2018-04-17 13:19:29 +0200

    Update pointer

--- Pointer/Pointer.cpp
+++ Pointer/Pointer.cpp
@@ -12,6 +12,8 @@ int main(){
 	cout << num << endl; 		// answer: 10
 	cout << ptr_num << endl; 	// answer: num's adress
 	cout << *ptr_num << endl;	// answer: 10
+	
+	char ch; cin >> ch; 		// Keep_window_open();
 
 return 0;
 }
